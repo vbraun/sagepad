3389a4160236def9b421073d4cc67899dbd9ded5
Volker Braun <vbraun.name@gmail.com>
2012-07-29 16:33:23 -0400

    configure flask from object, too

--- scripts/nat-port-setup.sh
+++ scripts/nat-port-setup.sh
@@ -14,13 +14,13 @@ frontend_ip='192.168.123.201'
 iptables -t nat $action PREROUTING -d ${external_ip} -i ${external_if} -p tcp -m tcp \
     --dport 2222 -j DNAT --to-destination ${frontend_ip}:22
 
-iptables -t nat $action PREROUTING -d ${external_ip} -i ${external_if} -p tcp -m tcp \
-    --dport 80 -j DNAT --to-destination ${frontend_ip}:80
+#iptables -t nat $action PREROUTING -d ${external_ip} -i ${external_if} -p tcp -m tcp \
+#    --dport 80 -j DNAT --to-destination ${frontend_ip}:80
 
 iptables -t nat $action PREROUTING -d ${external_ip} -i ${external_if} -p tcp -m tcp \
     --dport 2223 -j DNAT --to-destination ${backend_ip}:22
 
 
 iptables $action FORWARD -d ${frontend_ip} -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
-iptables $action FORWARD -d ${frontend_ip} -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT
+# iptables $action FORWARD -d ${frontend_ip} -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT
 iptables $action FORWARD -d ${backend_ip}  -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
--- secure/flask_config.py
+++ secure/flask_config.py
@@ -1,2 +1,4 @@
 SECRET_KEY='development key'
 SESSION_COOKIE_NAME='SagePad'
+DEBUG=True
+
