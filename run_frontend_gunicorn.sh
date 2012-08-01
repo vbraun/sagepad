26f4efd4327f78f6ae1d111dd36992a3bffaa94b
Volker Braun <vbraun.name@gmail.com>
2012-08-01 15:23:59 -0400

    Added logging to gunicorn runner

--- run_frontend_gunicorn.sh
+++ run_frontend_gunicorn.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-./bin/python/bin/gunicorn --access-logfile=- --error-logfile=- -w 4 -b frontend:8080 run_frontend:app
+./bin/python/bin/gunicorn \
+    --access-logfile=log/gunicorn_access.log \
+    --error-logfile=log/gunicorn_error.log \
+    -w 4 -b frontend:8080 run_frontend:app
---
+++ run_frontend_insecure.py
@@ -0,0 +1,12 @@
+#!./bin/python/bin/python
+
+import os, sys
+sys.path.append('src')
+sys.path.append('secure')
+
+from sagepad.frontend.app import app
+# app.config.from_pyfile(os.path.join(os.getcwd(), 'secure', 'flask_config.py'))
+app.config.from_object('flask_config')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080, host='frontend')
