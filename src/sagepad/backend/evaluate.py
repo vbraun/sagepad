e7146855c4be550d1383dda117f2e3626a23f11e
Volker Braun <vbraun.name@gmail.com>
2012-08-01 15:13:46 -0400

    evaluation on backend works now

--- src/sagepad/backend/evaluate.py
+++ src/sagepad/backend/evaluate.py
@@ -24,23 +24,24 @@ def evaluate_command(command, suffix, pad_input, timeout=10, sizelimit=1*1024*10
     outputfile = os.path.join(tmpdir, 'pad_output'+suffix)
     with open(inputfile, 'w') as f:
         f.write(pad_input)
-
-    f = open(outputfile, 'w')
-    cmd = subprocess.Popen(command+[inputfile], close_fds=True,
-                           stdout=f,
-                           stderr=subprocess.STDOUT)
+    os.environ['DOT_SAGE'] = os.path.join(tmpdir, 'dot_sage')
+    os.environ['CCACHE_DISABLE'] = 'true'
+    os.umask(0o0077)
+    with open(outputfile, 'w') as f:
+        cmd = subprocess.Popen(command+[inputfile], close_fds=True,
+                               stdout=f,
+                               stderr=subprocess.STDOUT)
     
-    # wait for any child to quit
-    oldhandler = signal.signal(signal.SIGALRM, my_sigalarm_handler)
-    signal.alarm(timeout)
-    try:
-        cmd.wait()
-    except AlarmException:
-        cmd.kill()
-    finally:
-        signal.signal(signal.SIGALRM, oldhandler)
-        signal.alarm(0)
-        f.close()   # open(outputfile, 'w')
+        # wait for any child to quit
+        oldhandler = signal.signal(signal.SIGALRM, my_sigalarm_handler)
+        signal.alarm(timeout)
+        try:
+            cmd.wait()
+        except AlarmException:
+            cmd.kill()
+        finally:
+            signal.signal(signal.SIGALRM, oldhandler)
+            signal.alarm(0)
 
     with open(outputfile, 'r') as f:
         pad_output = f.read(sizelimit)
